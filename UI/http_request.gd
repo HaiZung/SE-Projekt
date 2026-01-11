e02b5e407b4faf80141ad576dccd97ce77f0ac45
Isabell <igr45225@stud.hs-furtwangen.de>
2026-01-11 13:15:45 +0100

    post requests in godot

--- UI/http_request.gd
+++ UI/http_request.gd
@@ -16,7 +16,13 @@ func _ready():
 	#print(await request_robot_states())
 	#print(await request_stations())
 	#print(await request_train_id())
-	#print(await get_status_for_robot(1))
+	#print(await request_packages())
+	#add_robotId (420)
+	#add_package(1,2.0,"2x2x3",4)
+	#add_robotstates(1, "Batterie leer")
+	#add_stations("Station_34")
+	add_trainid(334, "12:22", "13:22", "k1", "k5")
+
 	pass
 
 func request_http(path: String) -> String:
@@ -94,3 +100,57 @@ func get_packages_for_robot(robotID: int):
 		return ["No package for Robot ID"]
 	
 	return packages
+
+func post_http(path: String, payload: String) -> void:
+	print("Start request for: " + url + path)
+
+	var headers = [
+		"Content-Type: application/json"
+	]
+
+	var err = request(
+		url + path,
+		headers,
+		HTTPClient.METHOD_POST,
+		payload
+	)
+
+	if err != OK:
+		push_error("Request failed: %d" % err)
+		print("Request failed: %d" % err)
+		return
+
+	var result = await request_completed
+	var response_code = result[1]
+
+	if response_code < 200 or response_code >= 300:
+		push_error("HTTP Error %d" % response_code)
+		print("HTTP Error %d" % response_code)
+		return
+
+	print("request succsess"+ url + path)
+
+func add_robotId(roboter_id: int)-> void:
+	var data= {"roboter_id": roboter_id}
+	var json= JSON.stringify(data)
+	await post_http(path_robot_id, json)
+
+func add_package(paketnummer: int, gewicht: float, masse: String, roboter_id: int)-> void:
+	var data= {"paketnummer": paketnummer, "gewicht": gewicht, "masse": masse, "roboter_id": roboter_id}
+	var json= JSON.stringify(data)
+	await post_http(path_packages, json)
+
+func add_robotstates( roboter_id: int, status: String)-> void:
+	var data= {"Status": status, "roboter_id": roboter_id}
+	var json= JSON.stringify(data)
+	await post_http(path_robot_states, json)
+
+func add_stations(station: String)-> void:
+	var data= {"Station": station}
+	var json= JSON.stringify(data)
+	await post_http(path_stations, json)
+
+func add_trainid(zugnummer: int, start: String, stop: String,von: String, bis: String )-> void:
+	var data= {"Zugnummer": zugnummer, "Start": start, "Stop": stop, "Von": von, "Bis": bis}
+	var json= JSON.stringify(data)
+	await post_http(path_train_id, json)
--- karte/backend/app/test_post.py
+++ karte/backend/app/test_post.py
@@ -6,6 +6,10 @@ def test_posts():
     # Data to be sent
     test_data = {
         "/robotid": {"roboter_id": 99},
+        "/packages": {"paketnummer": 500, "gewicht": 1.2, "masse": "5x5x5", "roboter_id": 99},
+        "/robotstates": {"roboter_id": 99, "status": "charging"},
+        "/stations": {"station": "Station_Z"},
+        "/trainid": {"zugnummer": 10, "start": "18:00", "stop": "22:00", "von": "A", "bis": "Z"}
     }
 
     print("--- Testing POST Endpoints ---")
